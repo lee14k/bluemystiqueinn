796e2dac1c744f656be1c465970c5f6b465a5948
lee14k <kmkovach@mtu.edu>
2024-05-01 21:36:50 -0400

    working on ics output

--- pages/api/fetchAirbnbCalendar.js
+++ pages/api/fetchAirbnbCalendar.js
@@ -1,6 +1,5 @@
-// pages/api/syncAirbnbCalendars.js
 import axios from 'axios';
-import { fromURL } from 'node-ical';
+import { parseICS } from 'node-ical';
 
 const airbnbIcalUrls = [
     'https://www.airbnb.com/calendar/ical/1101512343384815926.ics?s=bb32f23311402755c2e0f6fcca34d32d',
@@ -13,21 +12,22 @@ const airbnbIcalUrls = [
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
-            const eventsArray = await Promise.all(airbnbIcalUrls.map(async url => {
-                const response = await axios.get(url);
-                const events = await fromURL(response.data);
+            const eventsArray = await Promise.all(airbnbIcalUrls.map(async (url) => {
+                const response = await axios.get(url, {
+                    headers: { Accept: 'text/calendar' },
+                    responseType: 'text'
+                });
+                const events = parseICS(response.data);  // Use parseICS to parse the fetched data
                 return events;
             }));
 
-            // Here, you would process these events to update your database
-            // For now, we just return them as a JSON response
             res.status(200).json(eventsArray);
         } catch (error) {
             console.error('Failed to fetch or parse the Airbnb calendars:', error);
             res.status(500).json({ error: 'Failed to sync calendars' });
         }
     } else {
-        res.setHeader('Allow', ['GET'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
