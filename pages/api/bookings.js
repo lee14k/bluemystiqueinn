c94897f8f5a72b922b472fd4d8fe2581fdd3a65f
lee14k <kmkovach@mtu.edu>
2024-05-21 11:05:47 -0400

    progress on booking

--- pages/api/bookings.js
+++ pages/api/bookings.js
@@ -1,19 +1,19 @@
-import { supabase } from "../../utils/supabase";
+import { supabase } from "@/utils/supabase";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { name, email, startDate, endDate, roomId, paymentStatus } = req.body;
+    const { firstName, lastName, email, startDate, endDate, roomId, paymentStatus } = req.body;
 
     try {
-      console.log("Received booking data:", { name, email, startDate, endDate, roomId, paymentStatus });
+      console.log("Received booking data:", { firstName, lastName, email, startDate, endDate, roomId, paymentStatus });
 
       const { data, error } = await supabase
-        .from("bookings")
-        .insert([{ name, email, start_date: startDate, end_date: endDate, room_id: roomId, payment_status: paymentStatus }]);
+        .from("booking")
+        .insert([{ guest_name_one: firstName, guest_name_two: lastName, email, start_date: startDate, end_date: endDate, room_name: roomId, payment_status: paymentStatus }]);
 
       if (error) {
         console.error("Error inserting booking:", error);
-        throw error;
+        return res.status(500).json({ error: error.message, details: error });
       }
 
       console.log("Booking saved successfully:", data);
