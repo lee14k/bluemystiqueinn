d6050174514059266a1b33475585055f2f466d57
lee14k <kmkovach@mtu.edu>
2024-05-24 00:50:27 -0400

    fe

--- components/HomeHeader.jsx
+++ components/HomeHeader.jsx
@@ -1,22 +1,28 @@
 import Image from 'next/image';
+import { Della_Respira } from 'next/font/google';
+const della = Della_Respira({ subsets: ['latin'], weight: '400' });
+
 const HomeHeader = () => {
-    return(
-    <div className='hh-bg flex flex-col justify-center items-center' >
-        <Image
-        src="/bmlogov2.png"
-        alt="Blue Mystique Inn logo"
-        width={400}
-        height={400}
-        className='text-timer'
-        />
-        <div className='flex flex-col justify-center items-center text-white text-timer'>
-        <h2 className="text-2xl my-2">Welcome to</h2>
-        <h1 className='text-8xl my-2'>The Blue Mystique Inn</h1>
-        <h2 className="text-2xl my-2">Boutique Inn in Manistique, Michigan</h2>
-        <button className="text-4xl my-2">Book your Stay</button>
-    </div>
-    </div>
-    )
-}
+
+    return (
+        <div className={`hh-bg flex flex-col justify-center items-center ${della.className}`}>
+            <Image
+                src="/bmlogov2.png"
+                alt="Blue Mystique Inn logo"
+                width={400}
+                height={400}
+                className="text-timer"
+            />
+            <div className="flex flex-col justify-center items-center text-white text-timer">
+                <h2 className="text-2xl my-2">Welcome to</h2>
+                <h1 className="text-8xl my-2">The Blue Mystique Inn</h1>
+                <h2 className="text-2xl my-2">Boutique Inn in Manistique, Michigan</h2>
+                <button className="text-4xl my-2 bg-white rounded-2xl px-12 py-2 text-neutral-800">
+                    Book your Stay
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default HomeHeader;
