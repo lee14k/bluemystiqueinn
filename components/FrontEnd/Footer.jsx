c76324a10b47461148c51af6c5a419fa049bba6c
Kailee Hamre <kmkovach@mtu.edu>
2024-06-08 12:01:23 -0400

    packages and footer

--- components/FrontEnd/Footer.jsx
+++ components/FrontEnd/Footer.jsx
@@ -1,16 +1,19 @@
 import { Della_Respira } from "next/font/google";
+import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
 const della = Della_Respira({ subsets: ["latin"], weight: "400" });
 const Footer = () => {
   return (
-    <div className={`footerwrapper `}>
-      <div >
+    <div className={`footerwrapper bg-orange-50 flex flex-col justify-center  `}>
+      <div className="mx-24" >
         <h1 className={`${della.className} text-6xl`}>Blue Mystique Inn</h1>
-        <p>123 Main St, Manistique, MI 49854</p>
-      </div>
-      <div className="flex flex-col">
-        <span>(906) 221-5371</span>
+        <p className="text-3xl">123 Main St, Manistique, MI 49854</p>
+        <div className="flex flex-col text-xl">
+        <span >(906) 221-5371</span>
         <span>bluemystiquerentals@gmail.com</span>
+        <FacebookTwoToneIcon fontSize="large"/>
       </div>
+      </div>
+     
     </div>
   );
 };
