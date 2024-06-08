085b346aab9f623e7ef89d65bf7766699a47ae37
Kailee Hamre <kmkovach@mtu.edu>
2024-06-08 11:44:18 -0400

    footer

--- components/FrontEnd/Footer.jsx
+++ components/FrontEnd/Footer.jsx
@@ -1,12 +1,17 @@
-const Footer =()=> {
-    return (
-        <div>
-            <div className="grid grid-cols-2">
-                <h1>Blue Mystique Inn</h1>
-                <p>123 Main St, Manistique, MI 49854</p>
-                <p>906-555-1234</p>
-                </div>
-        </div>
-    )
-}
-export default Footer
+import { Della_Respira } from "next/font/google";
+const della = Della_Respira({ subsets: ["latin"], weight: "400" });
+const Footer = () => {
+  return (
+    <div className={`footerwrapper `}>
+      <div >
+        <h1 className={`${della.className} text-6xl`}>Blue Mystique Inn</h1>
+        <p>123 Main St, Manistique, MI 49854</p>
+      </div>
+      <div className="flex flex-col">
+        <span>(906) 221-5371</span>
+        <span>bluemystiquerentals@gmail.com</span>
+      </div>
+    </div>
+  );
+};
+export default Footer;
--- pages/book.js
+++ pages/book.js
@@ -4,16 +4,23 @@ import PayForm from "@/components/PayForm";
 import SelectRoom from "@/components/SelectRoom";
 import { Della_Respira } from "next/font/google";
 const della = Della_Respira({ subsets: ["latin"], weight: "400" });
+import Link from "next/link";
 export default function Book() {
   return (
     <div>
       <Navbar />
       <div className="flex flex-col justify-center items-center my-24">
         <h1 className={`text-6xl mx-24 ${della.className}`}>Book your Stay</h1>
-        <p>
-          We’re so happy to have you! Book your stay here. Questions? Send Us a
+        <p className="text-2xl my-2">
+          We’re so happy to have you! Questions? Send Us a
           Message.
         </p>
+        <Link href="/contact">
+          {" "}
+          <button className="text-xl my-2 text-sky-900 my-2 bg-sky-300 rounded-2xl px-12 py-2 text-neutral-800 rounded-2xl px-12 py-2 text-neutral-800">
+            Contact Us
+          </button>
+        </Link>
       </div>
       <SelectRoom />
     </div>
