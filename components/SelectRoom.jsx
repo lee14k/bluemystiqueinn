d861b02de30fb47e84d4a46aa9e0401936b49999
Kailee Hamre <kmkovach@mtu.edu>
2024-06-10 04:33:18 -0400

    thinking i got blocking mostly figured out

--- components/SelectRoom.jsx
+++ components/SelectRoom.jsx
@@ -40,7 +40,7 @@ const SelectRoom = () => {
     if (dateRange[0] && dateRange[1]) {
       filterAvailableRooms();
     }
-  }, [dateRange, rooms, bookingData]);
+  }, [dateRange, rooms, bookingData, unavailabilityData]);
 
   useEffect(() => {
     if (selectedDates[0] && selectedDates[1]) {
@@ -90,9 +90,10 @@ const SelectRoom = () => {
       console.error("Error fetching bookings:", error);
     }
   };
+
   const fetchUnavailabilityData = async () => {
     try {
-      const response = await fetch("/api/unavailability");
+      const response = await fetch("/api/get-unavailability");
       const data = await response.json();
       if (response.ok) {
         setUnavailabilityData(data);
@@ -103,6 +104,7 @@ const SelectRoom = () => {
       console.error("Error fetching unavailability data:", error);
     }
   };
+
   const filterAvailableRooms = () => {
     const newAvailableRooms = rooms.map((room) => {
       const isBooked = bookingData.some((booking) => {
@@ -116,10 +118,11 @@ const SelectRoom = () => {
         }
         return false;
       });
+
       const isUnavailable = unavailabilityData.some((unavailable) => {
         if (unavailable.room_id === room.id) {
           const unavailableStart = dayjs(unavailable.start_date);
-          const unavailableEnd = dayjs(unavailable.end_date);
+          const unavailableEnd = dayjs(unavailable.end_date)
           return (
             dateRange[0].isBefore(unavailableEnd) &&
             dateRange[1].isAfter(unavailableStart)
@@ -127,7 +130,11 @@ const SelectRoom = () => {
         }
         return false;
       });
-      return { ...room, availability: isBooked ? "Unavailable" : "Available" };
+
+      return {
+        ...room,
+        availability: isBooked || isUnavailable ? "Unavailable" : "Available",
+      };
     });
 
     setAvailableRooms(newAvailableRooms);
@@ -191,91 +198,91 @@ const SelectRoom = () => {
           />
         ))}
         <div className="flex flex-col gap-4">
-         <button
-        className="text-5xl h-1/3 w-full my-2 text-sky-900 my-2 bg-sky-300 rounded-2xl px-12 py-2 text-neutral-800 flex justify-center items-center"
-        onClick={handleProceed}
-        disabled={!selectedRoom}
-      >
-        Proceed to Booking
-      </button>
-      <div className="bg-sky-100 p-8 mb-4 h-1/2 flex flex-col justify-center  rounded-2xl">
-        <div className="flex justify-between mb-2 ">
-          <span className="font-bold text-3xl">Subtotal:</span>
-          <span className="text-3xl">${subtotal.toFixed(2)}</span>
-        </div>
-        <div className="flex justify-between mb-2">
-          <span className="font-bold text-2xl ">Sales Tax (6%):</span>
-          <span className="text-xl">${(subtotal * 0.06).toFixed(2)}</span>
-        </div>
-        <div className="flex justify-between font-bold text-lg pt-10">
-          <span className="text-4xl">Total:</span>
-          <span className="text-4xl">${total.toFixed(2)}</span>
-        </div>
+          <button
+            className="text-5xl h-1/3 w-full my-2 text-sky-900 my-2 bg-sky-300 rounded-2xl px-12 py-2 text-neutral-800 flex justify-center items-center"
+            onClick={handleProceed}
+            disabled={!selectedRoom}
+          >
+            Proceed to Booking
+          </button>
+          <div className="bg-sky-100 p-8 mb-4 h-1/2 flex flex-col justify-center rounded-2xl">
+            <div className="flex justify-between mb-2">
+              <span className="font-bold text-3xl">Subtotal:</span>
+              <span className="text-3xl">${subtotal.toFixed(2)}</span>
+            </div>
+            <div className="flex justify-between mb-2">
+              <span className="font-bold text-2xl">Sales Tax (6%):</span>
+              <span className="text-xl">${(subtotal * 0.06).toFixed(2)}</span>
+            </div>
+            <div className="flex justify-between font-bold text-lg pt-10">
+              <span className="text-4xl">Total:</span>
+              <span className="text-4xl">${total.toFixed(2)}</span>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-     
+
       <Modal
-  open={modalOpen}
-  onClose={handleClose}
-  aria-labelledby="room-details-title"
-  aria-describedby="room-details-description"
->
-  <Box
-    sx={{
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      width: { xs: '90%', sm: 400, md: 500 }, // Responsive width
-      bgcolor: "background.paper",
-      border: "2px solid #000",
-      boxShadow: 24,
-      p: { xs: 2, md: 4 }, // Responsive padding
-      maxHeight: { xs: '80vh', md: '90vh' }, // Max height for scrollable content
-      overflowY: 'auto', // Enable vertical scrolling
-    }}
-  >
-    {selectedRoomDetails && (
-      <>
-        <Typography id="room-details-title" variant="h6" component="h2">
-          {selectedRoomDetails.second_name}
-        </Typography>
-        <CardMedia
-          component="img"
-          height="140"
-          image={selectedRoomDetails.image}
-          alt={selectedRoomDetails.second_name}
-        />
-        <Typography id="room-details-description" sx={{ mt: 2 }}>
-          Occupancy: {selectedRoomDetails.occupancy}
-        </Typography>
-        <Typography id="room-details-rate" sx={{ mt: 2 }}>
-          Description: {selectedRoomDetails.room_description}
-        </Typography>
-        <Typography id="room-details-rate" sx={{ mt: 2 }}>
-          Rate: ${selectedRoomDetails.rate}
-        </Typography>
-        <Typography id="room-details-rate" sx={{ mt: 2 }}>
-          Amenities:
-          <ul>
-            {selectedRoomDetails.amenities.map((amenity, index) => (
-              <li key={index}>{amenity}</li>
-            ))}
-          </ul>
-        </Typography>
-        <Button
-          onClick={handleClose}
-          variant="contained"
-          color="primary"
-          sx={{ mt: 2 }}
+        open={modalOpen}
+        onClose={handleClose}
+        aria-labelledby="room-details-title"
+        aria-describedby="room-details-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: { xs: "90%", sm: 400, md: 500 }, // Responsive width
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: { xs: 2, md: 4 }, // Responsive padding
+            maxHeight: { xs: "80vh", md: "90vh" }, // Max height for scrollable content
+            overflowY: "auto", // Enable vertical scrolling
+          }}
         >
-          Close
-        </Button>
-      </>
-    )}
-  </Box>
-</Modal>
+          {selectedRoomDetails && (
+            <>
+              <Typography id="room-details-title" variant="h6" component="h2">
+                {selectedRoomDetails.second_name}
+              </Typography>
+              <CardMedia
+                component="img"
+                height="140"
+                image={selectedRoomDetails.image}
+                alt={selectedRoomDetails.second_name}
+              />
+              <Typography id="room-details-description" sx={{ mt: 2 }}>
+                Occupancy: {selectedRoomDetails.occupancy}
+              </Typography>
+              <Typography id="room-details-rate" sx={{ mt: 2 }}>
+                Description: {selectedRoomDetails.room_description}
+              </Typography>
+              <Typography id="room-details-rate" sx={{ mt: 2 }}>
+                Rate: ${selectedRoomDetails.rate}
+              </Typography>
+              <Typography id="room-details-rate" sx={{ mt: 2 }}>
+                Amenities:
+                <ul>
+                  {selectedRoomDetails.amenities.map((amenity, index) => (
+                    <li key={index}>{amenity}</li>
+                  ))}
+                </ul>
+              </Typography>
+              <Button
+                onClick={handleClose}
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+              >
+                Close
+              </Button>
+            </>
+          )}
+        </Box>
+      </Modal>
     </div>
   );
 };
