b29b743a01df97bf9089eec438f88ccf9e76d772
Kailee Hamre <kmkovach@mtu.edu>
2024-06-10 03:57:02 -0400

    trying to fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/favicon.ico" />
       <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
