4ff2d11aa852f884f8d05af90970235d4bc0e8db
lee14k <kmkovach@mtu.edu>
2024-06-19 20:43:31 -0400

    chef's table on mobile

--- routes.js
+++ routes.js
@@ -2,4 +2,5 @@ export const routes = [
   { title: "Home", href: "/" },
   {title: "Book", href: "/book"},
   { title: "Contact", href: "/contact" },
+  {title:"Chef's Table", href:"/chefs-table"}, 
 ];
