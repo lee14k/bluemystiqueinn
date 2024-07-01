d9b205b7d45d9cf5440ba44532376188e5ba40da
lee14k <kmkovach@mtu.edu>
2024-07-01 09:26:34 -0400

    food page copy and events page

--- components/FoodCTA.jsx
+++ components/FoodCTA.jsx
@@ -12,21 +12,13 @@ const FoodCTA = () => {
             Gourmet dining experiences in Manistique, Michigan
           </h1>
           <p className="my-8 text-2xl mx-12">
-            Discover the refined ambiance and exceptional dining at the Blue
-            Mystique Inn in Manistique. This stunning venue offers the perfect
-            setting for your next gathering, whether it's a special event or a
-            business meeting. Chef Joshua will work closely with you to custom
-            design a mouthwatering menu that caters to your tastes and
-            preferences, ensuring an unforgettable culinary experience for you
-            and your guests. When you host your event at the Blue Mystique Inn,
-            you'll have exclusive access to the entire property, providing an
-            unparalleled level of privacy and sophistication. The elegant
-            surroundings, coupled with the exquisite cuisine, create an upscale
-            catered dining experience that will leave a lasting impression on
-            your guests. Don't miss the opportunity to elevate your next
-            gathering to new heights. Book your event at the Blue Mystique Inn
-            today and prepare to be enchanted by the sheer elegance and culinary
-            excellence that await you.
+            Chefs Table is a dinner feature exclusive to our guests. “A culinary
+            experience like no other! Featuring different worldly fusions in a
+            tasting menu with an enhanced, modern twist. We are proud to
+            showcase as many local ingredients to give you an organic U.P.
+            dining experience that will make your stay at The Blue Mystique Inn
+            a memorable one.” - Chef Joshua Questions? Feel free to call (906)
+            221-5371
           </p>
           <div className={`${della.className}`}>
             <button className="bg-white rounded-2xl px-12 py-2 mx-16 my-10 text-4xl">
